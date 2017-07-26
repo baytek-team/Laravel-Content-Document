ba96966650aff500e2122471c2b8b132d72c1189
Sarah Marinoff <sarah@baytek.ca>
2017-07-26 13:16:00 -0400

    Added missing route

--- src/Routes/admin/document.php
+++ src/Routes/admin/document.php
@@ -1,9 +1,18 @@
 <?php
 
 Route::group(['as' => 'document.'], function () {
+
+	/**
+	 * Folder Routes
+	 */
 	Route::get('folder/{folder}/child', 'FolderController@create')
 		->name('folder.create.child');
-
 	Route::resource('folder', FolderController::class);
+
+	/**
+	 * File Routes
+	 */
+	Route::get('file/{file}/download', 'FileController@download')
+		->name('file.download');
 	Route::resource('file', FileController::class);
 });
